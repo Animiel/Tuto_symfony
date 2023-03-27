91a3bd01b0fd9f10135cdb817f665d2c3ebce5b9
Tuto_symfony <florian.leininger01@gmail.com>
2023-03-27 17:00:14 +0200

    + debut tuto symfony

---
+++ src/Controller/EntrepriseController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Entreprise;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\Persistence\ManagerRegistry;        //pourquoi pas doctrine/persistence comme dans vidéo ?
+
+class EntrepriseController extends AbstractController
+{
+    #[Route('/entreprise', name: 'app_entreprise')]
+    public function index(ManagerRegistry $doctrine): Response
+    {
+        // on récupère les entreprises de la BDD
+        $entreprises = $doctrine->getRepository(Entreprise::class)->findAll();
+        return $this->render('entreprise/index.html.twig', [
+            'entreprises' => $entreprises,
+        ]);
+    }
+
+    #[Route("/entreprise/{id}", name: "show_entreprise")]
+    public function show() {
+        $entreprise = "";
+        return $this->render('entreprise/show.html.twig', [
+            'entreprise' => $entreprise,
+        ]);
+    }
+}
