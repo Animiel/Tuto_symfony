1c13c0036408c9ad49a990886df97559b545657f
Tuto_symfony <florian.leininger01@gmail.com>
2023-03-29 15:11:48 +0200

    + ajout options edit et delete

--- src/Controller/EmployeController.php
+++ src/Controller/EmployeController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\Employe;
+use App\Form\EmployeType;
 use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -17,6 +19,44 @@ class EmployeController extends AbstractController
         return $this->render('employe/index.html.twig', [
             'employes' => $employes
         ]);
+    }
+
+    #[Route("/employe/add", name: "add_employe")]
+    #[Route("/employe/{id}/edit", name: 'edit_employe')]
+    public function add(ManagerRegistry $doctrine, Employe $employe = null, Request $request): Response {
+
+        if (!$employe) {
+            $employe = new Employe();
+        }
+
+        $form = $this->createForm(EmployeType::class, $employe);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $employe = $form->getData();
+            $entityManager = $doctrine->getManager();
+            //prepare
+            $entityManager->persist($employe);
+            //insert into (execute)
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_employe');
+        }
+
+        //vue pour afficher le formulaire d'ajout
+        return $this->render('employe/add.html.twig', [
+            'formAddEmploye' => $form->createView(),
+            'edit' => $employe->getId()
+        ]);
+    }
+
+    #[Route("/employe/{id}/delete", name: "delete_employe")]
+    public function delete(ManagerRegistry $doctrine, Employe $employe): Response {
+        $entityManager = $doctrine->getManager();
+        $entityManager->remove($employe);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('app_employe');
     }
 
     #[Route('/employe/{id}', name: 'show_employe')]
--- src/Controller/EntrepriseController.php
+++ src/Controller/EntrepriseController.php
@@ -23,7 +23,13 @@ class EntrepriseController extends AbstractController
     }
 
     #[Route("/entreprise/add", name: "add_entreprise")]
+    #[Route("/entreprise/{id}/edit", name: "edit_entreprise")]
     public function add(ManagerRegistry $doctrine, Entreprise $entreprise = null, Request $request): Response {
+
+        if (!$entreprise) {
+            $entreprise = new Entreprise();
+        }
+
         $form = $this->createForm(EntrepriseType::class, $entreprise);
         $form->handleRequest($request);
 
@@ -40,8 +46,18 @@ class EntrepriseController extends AbstractController
 
         //vue pour afficher le formulaire d'ajout
         return $this->render('entreprise/add.html.twig', [
-            'formAddEntreprise' => $form->createView()
+            'formAddEntreprise' => $form->createView(),
+            'edit' => $entreprise->getId()
         ]);
+    }
+
+    #[Route("/entreprise/{id}/delete", name: "delete_entreprise")]
+    public function delete(ManagerRegistry $doctrine, Entreprise $entreprise): Response {
+        $entityManager = $doctrine->getManager();
+        $entityManager->remove($entreprise);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('app_entreprise');
     }
 
     #[Route("/entreprise/{id}", name: "show_entreprise")]
